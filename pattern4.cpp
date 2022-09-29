c07ab1e2ca44fe0e82a9df72253e11cf73eb9f5d
Tushar <tusharbarman123@gmail.com>
2022-09-29 15:02:47 +0530

    2nd
    Signed-off-by: Tushar <tusharbarman123@gmail.com>

--- pattern4.cpp
+++ pattern4.cpp
@@ -5,9 +5,14 @@ int main()
 {
     for(int i =5; i>=1; i--)
     {
-        for(int j=1; j<=i; j++)
+        for (int k = i; k <5 ; k++)
         {
-            cout<<j;
+            cout<<" ";
+        }
+        
+        for(int j=2*i-1; j>=1; j--)
+        {
+            cout<<"*";
         }
         cout<<endl;
     }
